e005719d8a891dbe002949d6d51de34d03fa8e62
Robert Stomski <stomski.robert@gmail.com>
2024-08-25 11:43:02 -0600

    first query selectors

--- tinyApp/scripting/index.js
+++ tinyApp/scripting/index.js
@@ -1,2 +1,4 @@
 console.log("hello world");
-const testElement = document.querySelector(".myclass");
+const testElement = document.querySelector("#testingh2");
+console.log(testElement, "<test element");
+testElement.innerText = "fuck yes.";
