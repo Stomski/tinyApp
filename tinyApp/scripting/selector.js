5af3a1b3d04b226e3d934dcdd82c753854fbbb9f
Robert Stomski <stomski.robert@gmail.com>
2024-08-31 07:03:03 -0600

    moving to an art/css branch

--- tinyApp/scripting/selector.js
+++ tinyApp/scripting/selector.js
@@ -1,14 +1,5 @@
 /*
-This file is designed as the engine that will switch the viewable section of this webpage.
-
-my idea is to create individual sections, that will all inhabit the full page of any browser but that are either hidden or visible based on a piece of state that exists in i suppose either local storage or just in the DOM
-
-how do i keep track of that variable?
-
-    - i need to gather all the elements, the sections
-    - i can create a function that will set the given sections visible status to shown, and any other section to hidden
-    -
-
+PAGE SELECTOR ENGINE
 */
 
 import * as canvas from "./canvas.js";
