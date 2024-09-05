737895d4c7223bbc7f1377da240a167703c574b5
Robert Stomski <stomski.robert@gmail.com>
2024-09-05 11:48:06 -0600

    notes persisting locally

--- tinyApp/scripting/notes.js
+++ tinyApp/scripting/notes.js
@@ -1,4 +1,22 @@
 export function populateNotes() {
+  console.log(
+    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  );
+
+  const testTrigger = (e) => {
+    console.log(e.target.value);
+    localStorage.setItem("notes", e.target.value);
+  };
+  const textArea = document.querySelector("#notes-scroll");
+  textArea.addEventListener("input", testTrigger);
+
   const notesInStorage = localStorage.getItem("notes");
-  console.log("notes", notesInStorage);
+  if (notesInStorage) {
+    textArea.value = notesInStorage;
+  } else {
+    textArea.value =
+      "Think of this as your digital typewriter scroll. It exists locally on your computer, and will persist unless you clear your cache. Try not to break anything, YOUR computer stores this not mine.";
+  }
+
+  console.log("notes from storage>>>", notesInStorage);
 }
